b6ea25a25f0ed256ba7692ac1959a2cccf558ae6
Michael Jarvis <michael.jarvis@wegotpop.com>
2021-10-10 11:56:16 +0100

    camUrl

--- react_app/src/components/Cams.js
+++ react_app/src/components/Cams.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import {Header} from './Header';
+import {useAuthContext} from "../contexts/AuthContext";
 
 function Cams(props) {
+
+    const {authState} = useAuthContext();
+
     return (
         <>
         <Header text='Cams' back={'/'}/>
         <div className={"flex-col-center container"}>
-            <div className="cam-screen"><div className="cam-title"><h3>Driveway:</h3></div><img src={"https://api.smarthome.mjfullstack.com/cam"} className={"cam-feed"} alt={"cam_feed"}/></div>
+            <div className="cam-screen"><div className="cam-title"><h3>Driveway:</h3></div><img src={authState.camUrl} className={"cam-feed"} alt={"cam_feed"}/></div>
         </div>
         </>
     );
