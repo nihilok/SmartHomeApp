438a398513e625f57941d2f9a6f0df1789225f7f
nihilok <mjfullstack@gmail.com>
2021-11-14 15:12:03 +0000

    help button

--- front-end-v2/src/components/HelpButton/HelpButton.tsx
+++ front-end-v2/src/components/HelpButton/HelpButton.tsx
@@ -1,14 +1,37 @@
 import * as React from "react";
 import './help-button.css';
-import { IconButton, Tooltip } from "@mui/material";
+import {ClickAwayListener, IconButton, Tooltip} from "@mui/material";
 import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 
 export function HelpButton() {
+    const [open, setOpen] = React.useState(false);
+
+  const handleTooltipClose = () => {
+    setOpen(false);
+  };
+
+  const handleTooltipOpen = () => {
+    setOpen(true);
+  };
   return (
-    <Tooltip title="Long tap/hover on controls for help">
-      <IconButton className={'help-button'} color="primary" aria-label="help" component="div">
-        <HelpOutlineIcon />
-      </IconButton>
-    </Tooltip>
+      <ClickAwayListener onClickAway={handleTooltipClose}>
+            <div>
+              <Tooltip
+                PopperProps={{
+                  disablePortal: true,
+                }}
+                onClose={handleTooltipClose}
+                open={open}
+                disableFocusListener
+                disableHoverListener
+                disableTouchListener
+                title="Long tap/hover on controls for help"
+              >
+                <IconButton onClick={handleTooltipOpen} className={'help-button'} color="primary" aria-label="help" component="div">
+                    <HelpOutlineIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          </ClickAwayListener>
   );
 }
