d6906181a6273b44887d253af52ff0ef8515027f
nihilok <mjfullstack@gmail.com>
2021-11-21 13:58:14 +0000

    fix tg hook

--- front-end-v2/src/hooks/TelegramBot.tsx
+++ front-end-v2/src/hooks/TelegramBot.tsx
@@ -1,28 +1,33 @@
 import { useFetchWithToken } from "./FetchWithToken";
 
-export function useTelegramDebugMessage() {
-
-  function useTelegramBotToken() {
-    const fetch = useFetchWithToken();
-    const localToken = JSON.parse(
-      localStorage.getItem("telegram_bot_token") as string
-    );
-    if (localToken) {
+function useTelegramBotToken() {
+  const fetch = useFetchWithToken();
+  const localToken = JSON.parse(
+    localStorage.getItem("telegram_bot_token") || "null"
+  );
+  if (localToken)
+    if (localToken.token) {
       return localToken;
     }
-    fetch("/secrets/telegram-bot-token/").then((res) =>
-      res.json().then((data) => {
+  fetch("/secrets/telegram-bot-token/").then((res) =>
+    res.json().then((data) => {
+      if (res.status === 200) {
         localStorage.setItem("telegram_bot_token", JSON.stringify(data));
         return data;
-      })
-    );
-  }
+      }
+      console.error(data);
+    })
+  );
+  return { token: null, channel: null };
+}
 
+export function useTelegramDebugMessage() {
   const { token, channel } = useTelegramBotToken();
 
   async function sendMessage(message: string) {
     const url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${channel}&text=${message}`;
     fetch(url).catch((err) => console.log(err));
   }
+
   return sendMessage;
 }
