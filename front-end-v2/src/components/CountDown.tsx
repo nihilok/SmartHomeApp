a5e61f107613611529d2ed80fdab77d1ce228db2
nihilok <mjfullstack@gmail.com>
2021-11-21 14:03:16 +0000

    countdown format

--- front-end-v2/src/components/CountDown.tsx
+++ front-end-v2/src/components/CountDown.tsx
@@ -1,29 +1,26 @@
-import * as React from 'react';
+import * as React from "react";
 
 interface Props {
-    endTime: number;
+  endTime: number;
 }
 
-export function CountDown({endTime}: Props) {
+export function CountDown({ endTime }: Props) {
+  const [remaining, setRemaining] = React.useState("");
 
-    const [remaining, setRemaining] = React.useState('')
+  React.useEffect(() => {
+    function countDown() {
+      const timeLeft = endTime - Date.now() / 1000;
+      const minutes = Math.floor((timeLeft % (60 * 60 * 24)) / 60);
+      const seconds = Math.floor(timeLeft % 60);
+      setRemaining(
+        `${minutes}:${seconds > 10 ? seconds : `${0}${seconds}`} remaining`
+      );
+    }
+    countDown();
+    let interval: ReturnType<typeof setInterval>;
+    interval = setInterval(countDown, 1000);
+    return () => clearInterval(interval);
+  }, [endTime]);
 
-    React.useEffect(() => {
-        function countDown() {
-            const timeLeft = endTime - Date.now()/1000;
-            const minutes = Math.floor(timeLeft % (60 * 60 * 24) / (60));
-            const seconds = Math.floor(timeLeft % (60));
-            setRemaining(`${minutes}:${seconds} remaining`)
-        }
-        countDown()
-        let interval: ReturnType<typeof setInterval>
-        interval = setInterval(countDown, 1000)
-        return ()=>clearInterval(interval)
-    }, [endTime])
-
-    return (
-        <div>
-            {remaining}
-        </div>
-    );
+  return <div>{remaining}</div>;
 }
