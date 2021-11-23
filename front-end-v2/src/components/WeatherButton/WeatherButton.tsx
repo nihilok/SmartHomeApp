020cc778d4abc99f155fd20a538ee1d875ed51e5
nihilok <mjfullstack@gmail.com>
2021-11-23 10:19:35 +0000

    sunrise sunset

--- front-end-v2/src/components/WeatherButton/WeatherButton.tsx
+++ front-end-v2/src/components/WeatherButton/WeatherButton.tsx
@@ -63,9 +63,9 @@ export function WeatherButton() {
   }, [weatherCallback]);
 
   React.useEffect(() => {
-      setSunrise(weather?.current?.sunrise)
-      setSunset(weather?.current?.sunset)
-  }, [weather?.current?.sunrise, weather?.current?.sunset])
+    setSunrise(weather?.current?.sunrise);
+    setSunset(weather?.current?.sunset);
+  }, [weather?.current?.sunrise, weather?.current?.sunset]);
 
   const content = (
     <>
@@ -77,7 +77,8 @@ export function WeatherButton() {
         {weather?.current?.weather[0]?.description}){" "}
       </p>
       <p>
-        Sunrise/set: {new Date(sunrise as number).getHours()}:{new Date(sunrise as number).getMinutes()}/{new Date(sunset as number).getHours()}:{new Date(sunset as number).getMinutes()}
+        Sunrise/set: {new Date(sunrise as number).toLocaleTimeString()}/
+        {new Date(sunset as number).toLocaleTimeString()}
       </p>
     </>
   );
